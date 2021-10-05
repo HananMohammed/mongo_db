8f1f189c535e5a5b82bcd7ecba1a91485f9969a2
HananMohammed <hananmohammed2468@gmail.com>
2021-10-05 11:58:26 +0200

    Intro to Mocha Testing

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const assert = require('assert')
+//Describe Tests
+describe('Some Demo Tests !!!!', function (){
+    // Create Tests
+    it("adds Two Numbers together", function () {
+        assert( 2 + 3 === 5);
+    })
+})
